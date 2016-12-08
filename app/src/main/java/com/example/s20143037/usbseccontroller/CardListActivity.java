6a24981e38ebd9266917bb3669e6d5267171b382
MikemoriKeisuke <mikekit41@gmail.com>
2016-12-08 14:00:22 +0900

    add
    new_add

---
+++ app/src/main/java/com/example/s20143037/usbseccontroller/CardListActivity.java
@@ -0,0 +1,53 @@
+package com.example.s20143037.usbseccontroller;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import java.util.ArrayList;
+
+public class CardListActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card_list);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView1);
+
+        mRecyclerView.setHasFixedSize(true);
+
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        // テストデータを作成
+        final ArrayList<String> myDataSet = new ArrayList<String>() {
+            {
+                add("USBsec1");
+                add("USBsec2");
+                add("USBsec3");
+            }
+        };
+
+        // アダプタを指定する
+        mAdapter = new UsbAdapter(this, myDataSet);
+        mRecyclerView.setAdapter(mAdapter);
+    }
+
+    public void intentConn(View v) {
+        Intent intent = new Intent(getApplication(), ConnectionActivity.class);
+        startActivity(intent);
+    }
+
+    public void intentSearchMap(View v) {
+        Intent intent = new Intent(getApplication(), SearchMapActivity.class);
+        startActivity(intent);
+    }
+
+}
---
+++ app/src/main/java/com/example/s20143037/usbseccontroller/UsbAdapter.java
@@ -0,0 +1,50 @@
+package com.example.s20143037.usbseccontroller;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class UsbAdapter extends RecyclerView.Adapter<UsbAdapter.ViewHolder> {
+    private LayoutInflater mLayoutInflater;
+    private ArrayList<String> mDataList;
+
+    public UsbAdapter(Context context, ArrayList<String> dataList) {
+        super();
+        mLayoutInflater = LayoutInflater.from(context);
+        mDataList = dataList;
+    }
+
+    // getViewのinfrateするところだけ取り出した感じ
+    @Override
+    public UsbAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = mLayoutInflater.inflate(R.layout.card_item, parent, false);
+        ViewHolder viewHolder = new ViewHolder(v);
+        return viewHolder;
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataList.size();
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        String data = (String) mDataList.get(position);
+        holder.text.setText(data);
+    }
+
+    // ViewHolder内でwidgetを割り当てる
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView text;
+
+        public ViewHolder(View v) {
+            super(v);
+            text = (TextView) v.findViewById(R.id.UsbNameView);
+        }
+    }
+}
