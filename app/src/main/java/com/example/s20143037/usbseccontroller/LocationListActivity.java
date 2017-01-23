153f4431ba8ae5577ef274d0153fccd0a2df9386
MikemoriKeisuke <mikekit41@gmail.com>
2017-01-23 14:18:43 +0900

    Revert "LocationListのバックボタン追加"
    This reverts commit d8eebbe4a0c31829c4acc8197a3593200d98cd20.

--- app/src/main/java/com/example/s20143037/usbseccontroller/LocationListActivity.java
+++ app/src/main/java/com/example/s20143037/usbseccontroller/LocationListActivity.java
@@ -1,14 +1,11 @@
 package com.example.s20143037.usbseccontroller;
 
 import android.location.Location;
-import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.widget.Adapter;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -28,7 +25,6 @@ public class LocationListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location_list);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         HashMap<String,String[]> locationMap=new HashMap<>();
         ListView listView=(ListView) findViewById(R.id.location_list);
         ArrayList<String> macList=MyService.allMacAddress();
@@ -55,11 +51,5 @@ public class LocationListActivity extends AppCompatActivity {
             e.printStackTrace();
         }
         return str;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        finish();
-        return super.onOptionsItemSelected(item);
     }
 }
